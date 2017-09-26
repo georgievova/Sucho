683246f784c962916efbbb3e9db0ae6fb69ee475
georgievova <shalkovairina17@gmail.com>
2017-09-26 09:46:33 +0200

    database preparation

--- databaze.R
+++ databaze.R
@@ -2,7 +2,7 @@ require(DBI)
 require(odbc)
 require(dplyr)
 require(dbplyr)
-require(sqldf)
+require(data.table)
 
 con <- dbConnect(odbc::odbc(), .connection_string = "Driver={PostgreSQL Unicode(x64)};", uid = 'postgres', pwd='postgres', database = 'dta_Sucho')
 
@@ -21,8 +21,28 @@ names(R) = gsub('\\.rds', '', names(R))
 RR = rbindlist(R, idcol = 'UPOV_ID')
 dbWriteTable(con, 'bil_daily', RR)
 
+setwd("C:/Users/Irina/Disk Google/1_ČZU/Sucho")
+
 BM <- readRDS('data/mbil/bilan_month.rds')
+
+BM.long <- dcast(BM, month+year+UPOV_ID~variable)
+
+mesice <- c("Leden","Únor","Březen","Duben","Květen","Červen",
+            "Červenec","Srpen","Září","Říjen","Listopad","Prosinec")
+
+seasons <- data.frame(month = c(1:12), seasons = c("Zíma", "Zíma", "Jaro", "Jaro", "Jaro", 
+                                                   "Léto", "Léto", "Léto","Podzim", "Podzim", "Podzim", "Zíma"))
+BM.long$m <- as.factor(BM.long$month)
+
+BM <- BM %>% left_join(seasons, by="month") 
+
 dbWriteTable(con, 'bil_monthly', BM)
+dbWriteTable(con, 'bil_monthly_long', BM.long)
+
+u <- readRDS('data/uzivani.rds')
+u <- u[complete.cases(X, Y)]
+u$DTM <- as.character(u$DTM)
+dbWriteTable(con, 'uzivani', u)
 
 QD <- readRDS('data/QD.rds')
 dbWriteTable(con, 'flow_rates_chmu', QD)
