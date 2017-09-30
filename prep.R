7b1f8f8bb14116ebe5e45c1e7fc2c85d456aff44
georgievova <shalkovairina17@gmail.com>
2017-10-01 00:31:54 +0200

    preparation data

--- prep.R
+++ prep.R
@@ -134,10 +134,16 @@ u = melt(u, id.vars = c("ICOC", "JEV", "CZ_NACE", "POVODI", "NAZEV", "ROCNI.MNOZ
 ))
 
 u$variable <- gsub("MVM*", "", u$variable)
+u$value <- gsub(",", ".", u$value)
 u$DTM <- paste0("01",str_sub(paste0("00",u$variable),-2,-1), u$ROK, sep="-")
 u$DTM <- as.Date(u$DTM, format = "%d-%m-%Y")
+u$value <- as.numeric(u$value)
 colnames(u)[13] <- "MESIC"
 colnames(u)[c(8,9)] <- c("X", "Y")
+colnames(u)[5] <- "NAZICO"
+
+u <- u[!is.na(u$X)|!is.na(u$Y),]
+
 
 #Saving
 #--------------------
