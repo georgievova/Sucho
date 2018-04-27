d38eec99fc0b68a345409fe45c64788528de9440
Irina Georgievová <shalkovairina17@gmail.com>
2018-04-27 21:56:39 +0200

    final

--- install.packages.R
+++ install.packages.R
@@ -1,24 +1,20 @@
 required_packages <- c("flexdashboard",
                        "leaflet",
-                       "rgdal",
+                       "htmltools",
                        "data.table",
                        "ggplot2",
-                       "GGally",
-                       "DBI",
-                       "odbc",
                        "dplyr",
-                       "dbplyr",
+                       "plyr",
                        "dygraphs",
                        "sp",
                        "knitr",
                        "shiny",
                        "DT",
+                       "psych",
                        "rgeos",
-                       "datasets",
                        "xts",
-                       "rmapshaper",
-                       "maptools",
-                       "stringr")
+                       "rhandsontable",
+                       "plotly")
 missing_packages <-
   required_packages[!(required_packages %in% installed.packages()[, "Package"])]
 if (length(missing_packages) > 0) {
