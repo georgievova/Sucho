49b9c62d150af59166596d9ffe9cd124b1ed2146
Irina <shalkovairina17@gmail.com>
2019-02-18 19:28:03 +0100

    + hydroGOF

--- install.packages.R
+++ install.packages.R
@@ -16,7 +16,8 @@ required_packages <- c("flexdashboard",
                        "rgeos",
                        "xts",
                        "rhandsontable",
-                       "plotly")
+                       "plotly",
+                       "hydroGOF")
 missing_packages <-
   required_packages[!(required_packages %in% installed.packages()[, "Package"])]
 if (length(missing_packages) > 0) {
